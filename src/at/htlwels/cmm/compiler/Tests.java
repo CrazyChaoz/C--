f14c8bab1359ec9ce58670a60e1eb53b148053bd
CrazyChaos <s-kempinger@gmx.at>
2018-07-02 11:21:42 +0200

    generate script generated

--- src/at/htlwels/cmm/compiler/Tests.java
+++ src/at/htlwels/cmm/compiler/Tests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+
 import at.htlwels.cmm.compiler.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,8 +18,8 @@ public class Tests {
     }
 
     @Test
-    public void testScan_token_Ident_x() throws UnsupportedEncodingException {
-        System.out.println("testScan_token_Ident_x");
+    public void tokenTest() throws UnsupportedEncodingException {
+        System.out.println("Der TokenTest");
         // Initialize
         String sContent = "<";
         InputStream is = new ByteArrayInputStream(sContent.getBytes("UTF-8"));
@@ -30,9 +31,10 @@ public class Tests {
         // Test
         Token result = instance.Scan();
         // Validate
-        assertNotNull( result );
-        assertEquals( expected.kind, result.kind );
-        assertNotNull( result.val );
-        assertEquals( expected.val, result.val );
+        assertNotNull(result);
+        assertNotNull(result.val);
+        assertEquals(expected.kind, result.kind);
+        assertEquals(expected.val, result.val);
     }
+
 }
