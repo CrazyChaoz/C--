d2dfccf61431e2cc1521ad2c0aa3a763a1ce8ee8
CrazyChaos <s-kempinger@gmx.at>
2018-07-03 16:56:11 +0200

    so much progress

--- src/at/htlwels/cmm/compiler/SyntaxTree.java
+++ src/at/htlwels/cmm/compiler/SyntaxTree.java
@@ -1,53 +1,49 @@
 package at.htlwels.cmm.compiler;
 
 
-public class SyntaxTree {
-    SyntaxNode currentNode = new SyntaxNode();
+import java.util.ArrayList;
+import java.util.List;
 
-    public SyntaxTree() {
-        openScope();
-    }
+public class SyntaxTree {
+    SyntaxScope globalSymbolScope =new SyntaxScope();
+    SyntaxScope currentScope = globalSymbolScope;
 
     /**
      * Create a new scope inside the current one and add it to the list of inner scopes.
      */
-    public void openScope() {
-        SyntaxNode newNode = new SyntaxNode();
-        currentNode.addChild(newNode);
-        newNode.parent = currentNode;
-        currentNode = newNode;
-
+    public void openScope(){
+        SyntaxScope newSymbolScope =new SyntaxScope();
+        currentScope.innerScopes.add(newSymbolScope);
+        newSymbolScope.outerScope= currentScope;
+        currentScope = newSymbolScope;
     }
 
     /**
      * Exit out of the current scope and return one level up the scope hierarchy.
      */
-    public void closeScope() {
-        currentNode = currentNode.parent;
+    public void closeScope(){
+        currentScope = currentScope.outerScope;
     }
 
     /**
      * Create a new node, define its name and type based on the parameters and add it to the current scope.
      */
-    public void addNode(Kind kind, Object value) {
-        System.out.println("Added Node with Kind."+kind+" and Value "+value);
-        SyntaxNode syntaxNode = new SyntaxNode();
+    public void addNode(Kind kind, Object value){
+        SyntaxNode syntaxNode =new SyntaxNode();
 
+        syntaxNode.kind=kind;
+        syntaxNode.value=value;
 
-        syntaxNode.kind = kind;
-        syntaxNode.value = value;
-        currentNode.addSibling(syntaxNode);
+        currentScope.addNode(syntaxNode);
     }
 
-    public void addNode(Kind kind) {
-        System.out.println("Added Node with Kind."+kind+" and no Value");
-        SyntaxNode syntaxNode = new SyntaxNode();
+    public void addNode(Kind kind){
+        SyntaxNode syntaxNode =new SyntaxNode();
 
+        syntaxNode.kind=kind;
+        syntaxNode.value=null;
 
-        syntaxNode.kind = kind;
-        syntaxNode.value = null;
-
-        currentNode.addSibling(syntaxNode);
+        currentScope.addNode(syntaxNode);
     }
 
 
@@ -56,44 +52,65 @@ public class SyntaxTree {
      */
     public void dumpTree() {
         System.out.println("Syntax Tree gets dumped");
-        currentNode.printMe();
+        globalSymbolScope.printMe("");
     }
 }
 
+class SyntaxScope {
+    SyntaxScope outerScope;
+    List<SyntaxScope> innerScopes=new ArrayList<>();
+
+    private SyntaxNode head, tail;
+
+    // Append a new node at the end of the node list.
+    public void addNode(SyntaxNode x) {
+
+        if (x != null) {
+            if (head == null)
+                head = x;
+            else
+                tail.next = x;
+
+            tail = x;
+        }
+    }
+
+    /**
+     * Traverse through all inner scopes recursively and print their nodes.
+     */
+    public void printMe(String einrueckung){
+
+        SyntaxNode symbolNode =this.head;
+
+        while (symbolNode !=null){
+            System.out.print(einrueckung);
+            symbolNode.printMe();
+            symbolNode = symbolNode.next;
+        }
+
+        for (SyntaxScope scope :innerScopes) {
+            scope.printMe(einrueckung+"\t");
+        }
+    }
+}
 
 class SyntaxNode {
-    SyntaxNode childHead, childTail, next, parent;
+    SyntaxNode next;
 
     Object value;
     Kind kind;
 
-    public void addSibling(SyntaxNode node) {
-        if (this.next == null)
-            next = node;
-    }
-
-    public void addChild(SyntaxNode node) {
-        if (childHead == null)
-            childHead = node;
-        else
-            childTail.next = node;
-
-        childTail = node;
-
-    }
 
     public void printMe() {
+
+
         if (value instanceof SymbolNode)
             ((SymbolNode) value).printMe();
         else if (value instanceof String)
-            System.out.println(value);
+            System.out.println(kind+", "+value);
         else
             System.out.println(kind);
 
-        if (childHead != null)
-            childHead.printMe();
-        else if (next != null)
-            next.printMe();
     }
 }
 
