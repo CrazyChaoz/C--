16659f60c7bfb2ed11c68271d195d3592caed90e
CrazyChaos <s-kempinger@gmx.at>
2018-07-13 09:18:42 +0200

    refactoring in testfile2

--- testfile2.c
+++ testfile2.c
@@ -1,19 +1,19 @@
 
-struct penniz{
+struct STRUUUKTUUUR{
     int length;
     char name;
 }
 
 void main() {
 
-penniz penis;
-penis.length = 26;
-penis.name = 'a';
+STRUUUKTUUUR struu;
+struu.length = 26;
+struu.name = 'a';
 
 //int uwu;
 //uwu = 15;
 //int i;
 //i = 10;
-print(penis.name);
+print(struu.length);
 //print(penis.length);
  }
